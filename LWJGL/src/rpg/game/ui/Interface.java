26ce1496e7b7a0a005b47316e31cbbc0377dbc80
Basti <basti.wolff-sebastian@web.de>
2015-07-23 15:26:54 +0200

    moving lootbox when open inventory while looting

--- LWJGL/src/rpg/game/ui/Interface.java
+++ LWJGL/src/rpg/game/ui/Interface.java
@@ -266,25 +266,33 @@ public class Interface {
 	private void renderLootDialog(){
 		// in actual state it is centered
 		
+		
 		int dialogWidth = 96;
 		int dialogHeight = 32 + 16*loot.size();
+		int dialogX;
+		
+		if(renderInventory){
+			dialogX = x + Game.SCREEN_WIDTH/2+5*40+2 + 50;
+		}else{
+			dialogX = x + Game.SCREEN_WIDTH/2 - dialogWidth/2;
+		}
 		
 		GL11.glDisable(GL11.GL_TEXTURE_2D);
 		
 		GL11.glColor3f(0.5f, 0.5f, 0.5f);
 		GL11.glBegin(GL11.GL_QUADS);
-		GL11.glVertex2f(x + Game.SCREEN_WIDTH/2 - dialogWidth/2 - 2, y + Game.SCREEN_HEIGHT/2 - dialogHeight - 2);
-		GL11.glVertex2f(x + Game.SCREEN_WIDTH/2 + dialogWidth/2 + 2, y + Game.SCREEN_HEIGHT/2 - dialogHeight - 2);
-		GL11.glVertex2f(x + Game.SCREEN_WIDTH/2 + dialogWidth/2 + 2, y + Game.SCREEN_HEIGHT/2 + dialogHeight + 2);
-		GL11.glVertex2f(x + Game.SCREEN_WIDTH/2 - dialogWidth/2 - 2, y + Game.SCREEN_HEIGHT/2 + dialogHeight + 2);
+		GL11.glVertex2f(dialogX - 2, y + Game.SCREEN_HEIGHT/2 - dialogHeight - 2);
+		GL11.glVertex2f(dialogX + dialogWidth + 2, y + Game.SCREEN_HEIGHT/2 - dialogHeight - 2);
+		GL11.glVertex2f(dialogX + dialogWidth +2, y + Game.SCREEN_HEIGHT/2 + dialogHeight + 2);
+		GL11.glVertex2f(dialogX - 2, y + Game.SCREEN_HEIGHT/2 + dialogHeight + 2);
 		GL11.glEnd();
 		
 		GL11.glColor3f(0.3f, 0.3f, 0.3f);
 		GL11.glBegin(GL11.GL_QUADS);
-		GL11.glVertex2f(x + Game.SCREEN_WIDTH/2 - dialogWidth/2, y + Game.SCREEN_HEIGHT/2 - dialogHeight);
-		GL11.glVertex2f(x + Game.SCREEN_WIDTH/2 + dialogWidth/2, y + Game.SCREEN_HEIGHT/2 - dialogHeight);
-		GL11.glVertex2f(x + Game.SCREEN_WIDTH/2 + dialogWidth/2, y + Game.SCREEN_HEIGHT/2 + dialogHeight);
-		GL11.glVertex2f(x + Game.SCREEN_WIDTH/2 - dialogWidth/2, y + Game.SCREEN_HEIGHT/2 + dialogHeight);
+		GL11.glVertex2f(dialogX, y + Game.SCREEN_HEIGHT/2 - dialogHeight);
+		GL11.glVertex2f(dialogX + dialogWidth, y + Game.SCREEN_HEIGHT/2 - dialogHeight);
+		GL11.glVertex2f(dialogX + dialogWidth, y + Game.SCREEN_HEIGHT/2 + dialogHeight);
+		GL11.glVertex2f(dialogX, y + Game.SCREEN_HEIGHT/2 + dialogHeight);
 		GL11.glEnd();
 		
 		GL11.glEnable(GL11.GL_TEXTURE_2D);
@@ -296,18 +304,18 @@ public class Interface {
 			
 			GL11.glColor3f(0.5f, 0.5f, 0.5f);
 			GL11.glBegin(GL11.GL_QUADS);
-			GL11.glVertex2f(x + Game.SCREEN_WIDTH/2 - 16, y + Game.SCREEN_HEIGHT/2 + dialogHeight - i*4 - i*32 - 32);
-			GL11.glVertex2f(x + Game.SCREEN_WIDTH/2 + 16, y + Game.SCREEN_HEIGHT/2 + dialogHeight - i*4 - i*32 - 32);
-			GL11.glVertex2f(x + Game.SCREEN_WIDTH/2 + 16, y + Game.SCREEN_HEIGHT/2 + dialogHeight - i*4 - i*32);
-			GL11.glVertex2f(x + Game.SCREEN_WIDTH/2 - 16, y + Game.SCREEN_HEIGHT/2 + dialogHeight - i*4 - i*32);
+			GL11.glVertex2f(dialogX + dialogWidth/2 - 16, y + Game.SCREEN_HEIGHT/2 + dialogHeight - i*4 - i*32 - 32);
+			GL11.glVertex2f(dialogX + dialogWidth/2 + 16, y + Game.SCREEN_HEIGHT/2 + dialogHeight - i*4 - i*32 - 32);
+			GL11.glVertex2f(dialogX + dialogWidth/2 + 16, y + Game.SCREEN_HEIGHT/2 + dialogHeight - i*4 - i*32);
+			GL11.glVertex2f(dialogX + dialogWidth/2 - 16, y + Game.SCREEN_HEIGHT/2 + dialogHeight - i*4 - i*32);
 			GL11.glEnd();
 			
 			GL11.glEnable(GL11.GL_TEXTURE_2D);
 			
 			GL11.glColor3f(1, 1, 1);
-			item.render(x + Game.SCREEN_WIDTH/2 - 16, y + Game.SCREEN_HEIGHT/2 + dialogHeight - i*4 - i*32 - 32);
+			item.render(dialogX + dialogWidth/2 - 16, y + Game.SCREEN_HEIGHT/2 + dialogHeight - i*4 - i*32 - 32);
 			String msg = Integer.toString(item.getStacks());
-			Font.render(msg, x + Game.SCREEN_WIDTH/2 + 16 - msg.length()* 8, y + Game.SCREEN_HEIGHT/2 + dialogHeight - i*4 - i*32 - 32);
+			Font.render(msg, dialogX + dialogWidth/2 + 16 - msg.length()* 8, y + Game.SCREEN_HEIGHT/2 + dialogHeight - i*4 - i*32 - 32);
 		}
 		
 	}
@@ -315,13 +323,20 @@ public class Interface {
 	private void processInput(int button, int mouseX, int mouseY){
 		int index = -1;
 		
+		int dialogWidth = 96;
 		int dialogHeight = 32 + 16*loot.size();
+		int dialogX;
+		if(renderInventory){
+			dialogX = x + Game.SCREEN_WIDTH/2+5*40+2 + 50;
+		}else{
+			dialogX = x + Game.SCREEN_WIDTH/2 - dialogWidth/2;
+		}
 		
 		for (Item item : loot) {
 			int i = loot.indexOf(item) + 1;
 			
-			if(	mouseX + x > x + Game.SCREEN_WIDTH/2 - 16
-				&& mouseX + x < x + Game.SCREEN_WIDTH/2 + 16
+			if(	mouseX + x > dialogX + dialogWidth/2 - 16
+				&& mouseX + x < dialogX + dialogWidth/2 + 16
 				&& mouseY + y > y + Game.SCREEN_HEIGHT/2 + dialogHeight - i*4 - i*32 - 32
 				&& mouseY + y < y + Game.SCREEN_HEIGHT/2 + dialogHeight - i*4 - i*32){
 				index = i-1;
