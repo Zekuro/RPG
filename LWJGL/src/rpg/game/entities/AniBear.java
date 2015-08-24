6fabb0c2f90d37c4f370ec3a8cf4a54bc54e2712
AkoyaP6618 <basti@basti-P6618>
2015-08-24 10:57:47 +0200

    some loot added.... found bug with equipping armor

--- LWJGL/src/rpg/game/entities/AniBear.java
+++ LWJGL/src/rpg/game/entities/AniBear.java
@@ -1,7 +1,9 @@
 package rpg.game.entities;
 
+import rpg.game.armor.boots.SimpleBoots;
 import rpg.game.items.Item;
 import rpg.game.items.Item.ItemObject;
+import rpg.game.items.Item.Tier;
 import rpg.game.player.Inventory;
 
 public class AniBear extends AnimatedEntity{
@@ -28,7 +30,21 @@ public class AniBear extends AnimatedEntity{
 	}
 	
 	public void createLoot(){
-		Inventory.add(Item.get(ItemObject.smallManaPotion), loot);
+		int randomNumber = (int) (Math.random()*100);
+		
+		if(randomNumber < 30){
+			Inventory.add(Item.get(ItemObject.smallManaPotion), loot);
+		}
+		
+		if(randomNumber >=30 && randomNumber < 60){
+			for(int i = 0; i <= (int) (Math.random()*3); i++){
+				Inventory.add(Item.get(ItemObject.smallHealthPotion), loot);
+			}
+		}
+		
+		if(randomNumber >= 95){
+			Inventory.add(new SimpleBoots(1, 3, Tier.T1), loot);
+		}
 	}
 	
 }
